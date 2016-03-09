51370d75927429118b7515121883ec2a61bb535c
starfish01 <patrick.labes@gmail.com>
2016-03-09 16:37:34 +1000

    Added files via upload

---
+++ myScript.js
@@ -0,0 +1,5 @@
+var warning = "If your feeling bored take a break";
+
+alert(warning);
+
+document.location.href="https://www.calm.com/";
---
+++ myScript1.js
@@ -0,0 +1,4 @@
+var prompt = "Your life is more interesting, dont waste it here"
+
+
+alert(prompt);
